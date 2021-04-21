582c2c18637d7bff22cd5c2bc6b2d2154d158dee
camaro <kbolukbas@gmail.com>
2021-04-21 19:05:29 +0300

    first commit

---
+++ src/main/java/com/example/validatingforminput/PersonForm.java
@@ -0,0 +1,48 @@
+package com.example.validatingforminput;
+
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class PersonForm {
+
+	@NotNull
+	@Size(min=2, max=30)
+	private String name;
+
+	@NotNull
+	@Min(18)
+	private Integer age;
+
+	private String cityName;
+
+
+	public String getCityName() {
+		return cityName;
+	}
+
+	public void setCityName(String cityName) {
+		this.cityName = cityName;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public String toString() {
+		return "Person(Name: " + this.name + ", Age: " + this.age + "," + this.cityName + ")";
+	}
+}
---
+++ src/main/java/com/example/validatingforminput/WebController.java
@@ -0,0 +1,50 @@
+package com.example.validatingforminput;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import javax.validation.Valid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Controller
+public class WebController implements WebMvcConfigurer {
+
+	private final ArrayList<City> cities = new ArrayList<>(
+		Arrays.asList(
+			new City("Ankara"), 
+			new City("İstanbul"), 
+			new City("İzmir")
+		)
+	);
+
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/results").setViewName("results");
+	}
+
+	@GetMapping("/")
+	public String showForm(PersonForm personForm, ModelMap modelMap) {
+		modelMap.addAttribute("cities", this.cities);
+
+		return "form";
+	}
+
+	@PostMapping("/")
+	public String checkPersonInfo(@Valid PersonForm personForm, BindingResult bindingResult) {
+		System.out.println(personForm.toString());
+		if (bindingResult.hasErrors()) {
+			return "form";
+		}
+
+		return "redirect:/results";
+	}
+}
